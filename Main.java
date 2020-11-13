aecbf9d74be73910342296a58aa634989a63f365
LGordy20 <liam.gordy@patelhs.org>
2020-11-13 21:26:33 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    int[] seats = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    String[] names = {"Jamal","Adam","Liam","Ian","Mateo","Sally","Joe","Seth","David","Chad","Brad","John","Tyler","Jana","William"};
+     for(int i = 0; i < 15; i++){
+       System.out.print("Jamal sits at seat 1");
+     }
+  }
+}
