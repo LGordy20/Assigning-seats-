9ed5ec69f1bc9c72bc5f97740f4904bc40b05192
LGordy20 <liam.gordy@patelhs.org>
2020-11-18 01:38:01 +0000

    Added Print statements and scanner

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
 class Main {
   public static void main(String[] args) {
-    int[] seats = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-    String[] names = {"Jamal","Adam","Liam","Ian","Mateo","Sally","Joe","Seth","David","Chad","Brad","John","Tyler","Jana","William"};
-     for(int i = 0; i < 15; i++){
-       System.out.print("Jamal sits at seat 1");
-     }
+    
+    int[] seatNumbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}; // Seat numbers
+    String[] names = {"Jake","John","Liam","Nico","Chris","Jimmy","Sarah","Tyler","Taylor","Evan","Alex","Patrick","Sean","Gideon","Anthony"}; 
+    for(int i = 0; i < names.length; i++){
+     System.out.println(names[i]); 
+     System.out.println(names[2]);
+     
+    }    
   }
 }
